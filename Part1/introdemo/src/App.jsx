c217286acc7e568bf084cfa50fde9a536c7cb79d
m-asikai <mikael.asikainen@metropolia.fi>
2024-12-14 20:30:38 +0200

    test

---
+++ Part1/introdemo/src/App.jsx
@@ -0,0 +1,53 @@
+import { useState } from 'react'
+
+const History = (props) => {
+  if (props.allClicks.length === 0){
+    return (
+      <div>
+        bress the button
+      </div>
+    )
+  }
+  return (
+    <div>
+      button press history: {props.allClicks.join(' ')}
+    </div>
+  )
+}
+
+const Button = ({ handleClick, text }) => (  <button onClick={handleClick}>    {text}  </button>)
+
+const App = () => {
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+  const [total, setTotal] = useState(0)
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    setLeft(left + 1)
+
+    setTotal(left + right)
+  }
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
+
+    setTotal(left + right)
+  }
+
+  return (
+    <div>
+      {left}
+      <Button onClick={handleLeftClick} text='left' />
+      <Button onClick={handleRightClick} text='right' />
+      {right}
+      <History allClicks={allClicks} />
+
+      <p>total {total}</p>
+    </div>
+  )
+}
+
+export default App
