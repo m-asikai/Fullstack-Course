9ccddac6e4023f9b90ceac58e900a9cb3261c494
m-asikai <asikainen.mikael@gmail.com>
2024-12-16 22:45:55 +0200

    Excercise 2.11

--- Part2/Phonebook/src/App.jsx
+++ Part2/Phonebook/src/App.jsx
@@ -1,19 +1,24 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import Shower from './Components/Shower';
 import Filter from './Components/Filter';
 import AddForm from './Components/AddForm';
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
   const [newFilter, setFilter] = useState('');
   const [personsToShow, setPersonsToShow] = useState(persons);
+
+  useEffect(() => {
+    axios
+        .get('http://localhost:3001/persons')
+        .then(response => {
+          setPersons(response.data);
+          setPersonsToShow(response.data);
+        })
+  }, []);
 
   const handleNewName = (e) => {
     setNewName(e.target.value);
@@ -61,8 +66,8 @@ const App = () => {
       <h2>Phonebook</h2>
       <Filter handleFilter={handleFilter} />
       <AddForm handleAdd={handleAdd} handleNewName={handleNewName} handleNewNumber={handleNewNumber}/>
-      <Shower personsToShow={personsToShow} />
       <h2>Numbers</h2>
+      <Shower personsToShow={personsToShow} />
     </div>
   )
 }
